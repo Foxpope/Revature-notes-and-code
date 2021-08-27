3a9867fc85cbf36152b07bb1d7d61fe256ebd90a
Foxpope <rashid@taleb.us>
2021-08-27 10:10:38 -0400

    first five questions solved

--- FirstProject/src/com/revature/HelloWorld.java
+++ FirstProject/src/com/revature/HelloWorld.java
@@ -1,10 +1,36 @@
 package com.revature;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello world!");
+		System.out.println("Hello world!");
+		run();
+	}
+	
+	static private void run() {
+		Scanner scan = new Scanner(System.in);
+        
+		int num = 0, den = 0;
+		
+        try {
+            num = scan.nextInt();
+            den = scan.nextInt();
+        } catch (InputMismatchException e) {
+        	e.printStackTrace();
+        	System.exit(0);
+        }
+        
+        try {
+        	System.out.print(num/den);
+        } catch (ArithmeticException e) {
+        	e.printStackTrace();
+        	System.exit(0);
+        }
+        
+        scan.close();
 	}
 
 }
