abe22b67d46517259c5fae3707c2b5d88f491e03
Foxpope <rashid@taleb.us>
2021-09-01 19:18:04 -0400

    still trying to add this stuff

--- JDBCDemo/src/main/resources/setup.sql
+++ JDBCDemo/src/main/resources/setup.sql
@@ -1,40 +1,70 @@
---save relevant sql scripts to your java project resources
-
+-- This is the script where I'm going to create the tables for my app
 
 DROP TYPE IF EXISTS rashidt.user_role CASCADE;
 CREATE TYPE rashidt.user_role AS ENUM ('Admin', 'Employee', 'Customer');
 
---this can be done with commas
-DROP TABLE IF exists rashidt.users, rashidt.accounts, rashidt.users_acc_joined, rashidt.applications CASCADE;
---this is just a demo (not *necessarily* a template for project 0)
-CREATE TABLE  rashidt.users (
---it does not have to to be the same names as in your User.java instance variables
+-- This is just a demo of JDBC connection, and YOUR projhect 0 script does not need to be exactly like this
+
+DROP TABLE IF EXISTS rashidt.users CASCADE;
+CREATE TABLE rashidt.users(
+	
+	-- it does NOT need to be the same name as your User.java instance variables
 	id SERIAL PRIMARY KEY,
 	username VARCHAR(50) NOT NULL UNIQUE,
 	pwd VARCHAR(50) NOT NULL,
 	user_role rashidt.user_role NOT NULL
 );
 
+DROP TABLE IF EXISTS rashidt.accounts CASCADE;
 CREATE TABLE rashidt.accounts (
 	id SERIAL PRIMARY KEY,
-	balance NUMERIC (50, 2) DEFAULT 0,
-	acc_owner integer NOT NULL REFERENCES rashidt.users(id),
-	active boolean DEFAULT FALSE --this IS determining whether the account has been activated
+	balance NUMERIC(50, 2) DEFAULT 0,
+	acc_owner INTEGER NOT NULL REFERENCES rashidt.users(id),
+	active BOOLEAN DEFAULT FALSE -- this IS determining whether the account has been activated
 );
 
---this seperate join table assists in the creation of a many to many table (because many people could own one account and vice versa)
-CREATE TABLE rashidt.users_acc_joined (
-	acc_owner integer NOT NULL REFERENCES rashidt.users(id),
-	account integer NOT NULL REFERENCES rashidt.accounts(id)
+-- This is demonstrating a MANY to MANY relationship....
+DROP TABLE IF EXISTS rashidt.users_account_jt CASCADE;
+CREATE TABLE rashidt.users_account_jt (
+	acc_owner INTEGER NOT NULL REFERENCES rashidt.users(id),
+	account INTEGER NOT NULL REFERENCES rashidt.accounts(id)
 );
 
---for applications to use the system
+-- this table represents pending applications of users who want to open an account
+DROP TABLE IF EXISTS rashidt.applications CASCADE;
 CREATE TABLE rashidt.applications (
-	id Serial PRIMARY KEY,
-	app_owner integer NOT NULL REFERENCES rashidt.users(id)
+	id SERIAL PRIMARY KEY,
+	app_owner INTEGER NOT NULL REFERENCES rashidt.users(id)
 );
 
+INSERT INTO rashidt.users(username, pwd, user_role) 
+	VALUES ('Larry', 'secret', 'Employee'),
+			('Mary', '1234', 'Customer'),
+			('Barry', 'dozer', 'Admin'),
+			('Kerry', '1wink', 'Customer'),
+			('Harry', 'plurg', 'Customer'),
+			('Perry', 'docat', 'Employee'),
+			('Jerry', 'orange', 'Admin');
+		
+SELECT * FROM rashidt.users;
+		
+INSERT INTO rashidt.accounts (balance, acc_owner)
+	VALUES (200, 6), (500, 6), (1000, 7), (250, 1);
 
+SELECT * FROM rashidt.accounts;
 
+INSERT INTO rashidt.users_account_jt
+	VALUES (1, 1),
+	(2, 2),
+	(6, 3),
+	(7, 4);
+
+SELECT * FROM rashidt.users_account_jt;
+
+-- JOIN from the accounts table and the users_account_jt WHERE the accowner_id is the same as the userId
+SELECT rashidt.accounts.id, rashidt.accounts.balance
+	FROM rashidt.accounts
+	INNER JOIN rashidt.users_account_jt
+	ON rashidt.accounts.id = rashidt.users_account_jt.acc_owner;
 
 
