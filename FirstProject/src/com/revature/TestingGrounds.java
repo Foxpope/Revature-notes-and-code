532d30db04425b36b2d15e91f94a909ddc010fcc
Foxpope <48967406+Foxpope@users.noreply.github.com>
2021-09-28 11:25:17 -0400

    added html stuff

--- FirstProject/src/com/revature/TestingGrounds.java
+++ FirstProject/src/com/revature/TestingGrounds.java
@@ -1,18 +1,21 @@
 package com.revature;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+import javax.print.attribute.SetOfIntegerSyntax;
+
 public class TestingGrounds {
 	public static void main(String[] args) {
-		thing(356);
+		
 	}
 
 	public static void thing(int i) {
-		String s = "1";
 		
-		System.out.println(s.toUpperCase());
 	}
 }
